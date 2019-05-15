3f4220c5a8badcf10db0042f12387c21affbc9dd
incep <vegard1989@gmail.com>
2019-05-15 13:11:28 +0200

    implemented command line args

--- src/com/Sematek/PressureArchiver/Main.java
+++ src/com/Sematek/PressureArchiver/Main.java
@@ -6,8 +6,17 @@ class Main {
 
     public static void main(String[] args) {
 
+        if(args.length == 0)
+        {
+            System.out.println("Proper Usage is: archiver.jar subscribeTopic publishTopic");
+            System.out.println("Using default values, \"pressure/#\", \"debug/archiver\"");
+            args = new String[2];
+            args[0] = "pressure/#";
+            args[1] = "debug/archiver";
+        }
+        
         Archiver archiver = new Archiver();
-        Subscriber s = new Subscriber(archiver, "pressure/#", "debug/archiver"
+        Subscriber s = new Subscriber(archiver, args[0], args[1]
                 + new Random().nextInt(100));
         s.connect();
     }
